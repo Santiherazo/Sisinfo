10f5156e255d0a9d1d4a1915e3f1b75445f41300
Santiherazo <113258688+Santiherazo@users.noreply.github.com>
2024-04-27 10:00:40 -0500

    Update sisinfo.php

--- includes/sisinfo.php
+++ includes/sisinfo.php
@@ -1,5 +1,8 @@
 <?php
 
+# Version
+define('__WEBENGINE_VERSION__', '1.0.0');
+
 # Set Encoding
 @ini_set('default_charset', 'utf-8');
 
@@ -22,43 +25,84 @@ define('__BASE_URL__', SERVER_PROTOCOL.HTTP_HOST.__RELATIVE_ROOT__); // http(s):
 
 # Private Paths
 define('__PATH_INCLUDES__', __ROOT_DIR__.'includes/');
+define('__PATH_TEMPLATES__', __ROOT_DIR__.'templates/');
 define('__PATH_LANGUAGES__', __PATH_INCLUDES__ . 'languages/');
 define('__PATH_CLASSES__', __PATH_INCLUDES__.'classes/');
 define('__PATH_FUNCTIONS__', __PATH_INCLUDES__.'functions/');
 define('__PATH_MODULES__', __ROOT_DIR__.'modules/');
+define('__PATH_MODULES_USERCP__', __PATH_MODULES__.'usercp/');
+define('__PATH_EMAILS__', __PATH_INCLUDES__.'emails/');
+define('__PATH_CACHE__', __PATH_INCLUDES__.'cache/');
+define('__PATH_ADMINCP__', __ROOT_DIR__.'admincp/');
+define('__PATH_ADMINCP_INC__', __ROOT_DIR__.'admincp/inc/');
+define('__PATH_ADMINCP_MODULES__', __ROOT_DIR__.'admincp/modules/');
+define('__PATH_NEWS_CACHE__', __PATH_CACHE__.'news/');
+define('__PATH_NEWS_TRANSLATIONS_CACHE__', __PATH_NEWS_CACHE__.'translations/');
+define('__PATH_PLUGINS__', __PATH_INCLUDES__.'plugins/');
+define('__PATH_CONFIGS__', __PATH_INCLUDES__.'config/');
+define('__PATH_MODULE_CONFIGS__', __PATH_CONFIGS__.'modules/');
+define('__PATH_CRON__', __PATH_INCLUDES__.'cron/');
+define('__PATH_LOGS__', __PATH_INCLUDES__.'logs/');
+define('__PATH_GUILD_PROFILES_CACHE__', __PATH_CACHE__.'profiles/guilds/');
+define('__PATH_PLAYER_PROFILES_CACHE__', __PATH_CACHE__.'profiles/players/');
 
 # Public Paths
+define('__PATH_MODULES_RANKINGS__', __BASE_URL__.'rankings/');
 define('__PATH_ADMINCP_HOME__', __BASE_URL__.'admincp/');
 define('__PATH_IMG__', __BASE_URL__.'img/');
 define('__PATH_UPLOAD__PROFILE__',__PATH_IMG__.'uploads/profile');
 define('__PATH_UPLOAD__COVER__',__PATH_IMG__.'uploads/cover');
+define('__PATH_COUNTRY_FLAGS__', __PATH_IMG__.'flags/');
 define('__PATH_API__', __BASE_URL__.'api/');
+define('__PATH_ONLINE_STATUS__', __PATH_IMG__.'online.png');
+define('__PATH_OFFLINE_STATUS__', __PATH_IMG__.'offline.png');
 
-<?php
-session_start();
-require_once 'router.php'; // Asegúrate de que la ruta del archivo sea correcta
-$routes = require_once 'routes.php'; // Asegúrate de que la ruta del archivo sea correcta
+# Timezone
+//if(!@include_once(__PATH_CONFIGS__ . 'timezone.php')) throw new Exception('Could not load timezone.');
 
+# Load Libraries
+//if(!@include_once(__PATH_CLASSES__ . 'class.database.php')) throw new Exception('Could not load class (database).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.common.php')) throw new Exception('Could not load class (common).');
+if(!@include_once(__PATH_CLASSES__ . 'class.handler.php')) throw new Exception('Ups, algo salió mal');
+//if(!@include_once(__PATH_CLASSES__ . 'class.validator.php')) throw new Exception('Could not load class (validator).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.login.php')) throw new Exception('Could not load class (login).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.vote.php')) throw new Exception('Could not load class (vote).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.character.php')) throw new Exception('Could not load class (character).');
+//if(!@include_once(__PATH_CLASSES__ . 'phpmailer/autoload.php')) throw new Exception('Could not load class (phpmailer).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.rankings.php')) throw new Exception('Could not load class (rankings).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.news.php')) throw new Exception('Could not load class (news).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.plugins.php')) throw new Exception('Could not load class (plugins).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.profiles.php')) throw new Exception('Could not load class (profiles).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.credits.php')) throw new Exception('Could not load class (credits).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.email.php')) throw new Exception('Could not load class (email).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.account.php')) throw new Exception('Could not load class (account).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.connection.php')) throw new Exception('Could not load class (connection).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.castlesiege.php')) throw new Exception('Could not load class (castlesiege).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.cron.php')) throw new Exception('Could not load class (cron).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.cache.php')) throw new Exception('Could not load class (cache).');
+//if(!@include_once(__PATH_CLASSES__ . 'paypal/PaypalIPN.php')) throw new Exception('Could not load class (PayalIPN).');
+//if(!@include_once(__PATH_CLASSES__ . 'class.upload.php')) throw new Exception('Could not load class (upload).');
 
-$router = new Router();
+# Load Functions
+//if(!@include_once(__PATH_INCLUDES__ . 'functions.php')) throw new Exception('Could not load functions.');
 
-// Definimos las rutas públicas
-foreach ($routes['public'] as $route => $handler) {
-    $router->get($route, $handler);
+# Load Configs
+//$config = webengineConfigs();
+
+# Check if the website is installed
+/*if($config['webengine_cms_installed'] == false) {
+	header('Location: '.__BASE_URL__.'install/');
+	die();
 }
+*/
+# Template Paths
+define('__PATH_TEMPLATE_ROOT__', __PATH_TEMPLATES__ . $config['website_template'] . '/');
+define('__PATH_TEMPLATE__', __BASE_URL__ . 'templates/' . $config['website_template'] . '/');
+define('__PATH_TEMPLATE_IMG__', __PATH_TEMPLATE__ . 'img/');
+define('__PATH_TEMPLATE_CSS__', __PATH_TEMPLATE__ . 'css/');
+define('__PATH_TEMPLATE_JS__', __PATH_TEMPLATE__ . 'js/');
+define('__PATH_TEMPLATE_FONTS__', __PATH_TEMPLATE__ . 'fonts/');
 
-// Definimos las rutas privadas
-$router->setPrivateRoutes($routes['private']);
-
-// Asignamos el manejador de vistas
-$router->setViewHandler(function ($viewName) {
-    include 'views/' . $viewName . '.php';
-});
-
-// Manejar la solicitud
-$router->dispatch();
-?>
-
-
-
-
+# Handler Instance
+//$handler = new Handler();
+//$handler->loadPage();
